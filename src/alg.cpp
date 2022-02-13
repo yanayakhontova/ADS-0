4010c092c393f49051ac67dc4c3bb99808ccb858
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-02-13 23:18:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-if (a % b == 0)
-return b;
-if (b % a == 0)
-return a;
-if (a > b)
-return gcd(a%b, b);
-return gcd(a, b%a);
-
+    // поместить сюда текст реализации алгоритма (на С++)
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
